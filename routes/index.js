58a2a3afd6e047c64057c5e5c162c628f30226c7
yishail <yishai.levenglick@explibrisgroup.com>
2022-07-05 09:20:04 +0300

    added photo url

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getUserPicture', function(req, res, next){
+  const user = req.params.user;
+  const photo = {
+    photo: "https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg"
+  };
+  if(user) {
+    console.log('user');
+    photo.photo = 'https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg';
+  }
+  res.json(photo);
+});
+
 module.exports = router;
